fcd0ed2da4bd0575172d71b28af8443aa1db7454
ZED114 <144231906+ZED114@users.noreply.github.com>
2024-07-12 09:04:24 +0200

    Add files via upload

---
+++ Engelwood final/demo.js
@@ -0,0 +1,7 @@
+// script.js
+
+document
+  .getElementById("hamburger-menu")
+  .addEventListener("click", function () {
+    document.getElementById("mobile-menu").classList.toggle("active");
+  });
